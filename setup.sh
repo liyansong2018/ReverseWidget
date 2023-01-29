c1d9a193dc140a05796db32307705d850a80321d
liyansong2018 <1321993383@qq.com>
2023-01-29 17:58:45 +0800

    Define dependency version

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-pip3 install pyqt5==5.15.8 keystone-engine==5.15.8 capstone==5.0.0 -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip3 install pyqt5==5.15.7 keystone-engine==0.9.2 capstone==5.0.0 -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip3 install PyCryptodome==3.17 -i https://pypi.tuna.tsinghua.edu.cn/simple/
