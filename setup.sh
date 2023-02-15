3f0bbbcdc3568d225647bb88f3b2dd7d190087eb
liyansong2018 <1321993383@qq.com>
2023-02-15 17:01:49 +0800

    Fix pip error about peid and change fast pip source(ustc) for China IP

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-pip3 install pyqt5==5.15.7 keystone-engine==0.9.2 capstone==5.0.0 -i https://pypi.tuna.tsinghua.edu.cn/simple/
-pip3 install PyCryptodome==3.17 lxml==4.9.2 peid=1.2.7 -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip3 install pyqt5==5.15.7 keystone-engine==0.9.2 capstone==5.0.0 -i https://pypi.mirrors.ustc.edu.cn/simple
+pip3 install PyCryptodome==3.17 lxml==4.9.2 peid==1.2.7 -i https://pypi.mirrors.ustc.edu.cn/simple
